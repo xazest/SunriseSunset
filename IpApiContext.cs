b5c7e08a7f3a72bd80fee08e3c612956783ae803
admin228 <admin228@DESKTOP-NTL96GO>
2025-06-20 22:37:31 +0300

    .

--- IpApiContext.cs
+++ IpApiContext.cs
@@ -5,15 +5,20 @@ class IpApiContext
     [JsonProperty("lat")] public float Latitude { get; private set; }
     [JsonProperty("lon")] public float Longitude { get; private set; }
     [JsonProperty("timezone")] public string Timezone { get; private set; } = string.Empty;
+    [JsonProperty("proxy")] public bool IsProxy { get; private set; }
 
     private async Task GetGeoDataAsync()
     {
-        string url = $"http://ip-api.com/json/?fields=lat,lon,timezone";
+        string url = $"http://ip-api.com/json/?fields=lat,lon,timezone,proxy";
         var response = await HttpC.Client.GetStringAsync(url);
         var result = JsonConvert.DeserializeObject<IpApiContext>(response);
         XMapper.Map(result, this);
 
         Console.Clear();
+        if (IsProxy)
+        {
+            Console.WriteLine("VPN/Proxy detected, data can be incorrect.\n");
+        }
         Console.WriteLine($"Coordinates acquired\nLatitude - {Latitude}" +
                 $"\nLongitude - {Longitude}\n");
     }
